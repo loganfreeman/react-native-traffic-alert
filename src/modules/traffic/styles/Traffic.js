d436d99b9f7b37cc5d1a97788638a33d039955be
shanhong cheng <shanhong.cheng@utah.edu>
2017-09-11 12:57:13 -0600

    margin vertical

--- src/modules/traffic/styles/Traffic.js
+++ src/modules/traffic/styles/Traffic.js
@@ -27,7 +27,6 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
   button: {
-    width: 100,
     paddingHorizontal: 8,
     alignItems: 'center',
     justifyContent: 'center',
@@ -35,7 +34,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: 'row',
-    marginVertical: 20,
+    marginVertical: 10,
     backgroundColor: 'transparent',
   },
   buttonText: {
